036854c862975b11c366015b557bce7623217bc3
EcemAydn <ecemaydn15@gmail.com>
2023-04-03 08:44:16 +0300

    create card and accounts component

---
+++ src/components/AccountsComp.vue
@@ -0,0 +1,27 @@
+<script setup>
+import ButtonComp from "../components/ButtonComp.vue";
+
+defineProps({
+  accountsTitle: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+});
+</script>
+<template>
+  <div class="flex gap-2">
+    <img
+      src="https://i.pravatar.cc"
+      alt="Photo"
+      class="w-8 h-8 rounded-full mt-1"
+    />
+    <div>
+      <h3>{{ accountsTitle }}</h3>
+      <p class="text-blue-500">{{ location }}</p>
+      <p class="text-sm text-gray-500">Last seen: 1 hour ago</p>
+    </div>
+  </div>
+  <ButtonComp button-name="Edit Item" color="none" />
+</template>
---
+++ src/components/CardComp.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  title: {
+    type: [String, Number],
+    default: "",
+  },
+});
+</script>
+<template>
+  <div
+    class="w-fit h-fit rounded-lg shadow-md p-6 m-4 bg-white flex flex-col gap-4"
+  >
+    <slot name="image"></slot>
+    <p class="font-bold text-xl">{{ title }}</p>
+    <slot name="info"></slot>
+  </div>
+</template>
