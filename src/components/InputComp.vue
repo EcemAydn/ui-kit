08ae4908f05db7675a301c576cf792f7bb8f9f1f
EcemAydn <ecemaydn15@gmail.com>
2023-04-03 16:22:07 +0300

    edit input component

--- src/components/InputComp.vue
+++ src/components/InputComp.vue
@@ -1,31 +1,113 @@
 <script setup>
-import { ref } from "vue";
-const forId = ref(new Date().getTime());
+import { cva } from "class-variance-authority";
 
+const labelColor = cva(["pl-2"], {
+  variants: {
+    l_color: {
+      primary: "text-black",
+      secondary: "text-white",
+    },
+    l_size: {
+      xsmall: "text-xs",
+      small: "text-sm",
+      normal: "text-md",
+    },
+    l_position: {
+      primary: "absoulte bottom-2 left-1",
+    },
+  },
+});
+
+const input = cva(
+  ["peer outline-gray-700 w-full font-light border border-gray-300"],
+  {
+    variants: {
+      size: {
+        xsmall: "p-1 pl-2 text-sm",
+        small: "p-2 px-3 text-sm",
+        normal: "p-2 px-4",
+        large: "p-3 px-5",
+        xlarge: "p-4",
+      },
+      radius: {
+        none: "rounded-none",
+        small: "rounded-sm",
+        normal: "rounded-md",
+        large: "rounded-lg",
+        xlarge: "rounded-full",
+      },
+      color: {
+        primary: "bg-white",
+        secondary: "bg-gray-700 text-white",
+        gray: "bg-gray-100",
+      },
+    },
+  }
+);
 defineProps({
-  modelValue: {
+  label: {
     type: [String, Number],
   },
   type: {
     type: String,
     default: "text",
   },
-  label: {
+  modelValue: {
+    type: [String, Number],
+    default: null,
+  },
+  size: {
     type: String,
+    default: "normal",
+  },
+  color: {
+    type: String,
+    default: "none",
+  },
+  showSelect: {
+    type: Boolean,
+    default: false,
+  },
+  radius: {
+    type: String,
+    default: "normal",
+  },
+  l_color: {
+    type: String,
+    default: "primary",
+  },
+  l_size: {
+    type: String,
+    default: "small",
+  },
+
+  validator(value) {
+    return [
+      "xsmall",
+      "small",
+      "normal",
+      "large",
+      "xlarge",
+      "none",
+      "primary",
+      "gray",
+      "secondary",
+    ].includes(value.toLocaleLowerCase());
   },
 });
-defineEmits(["update:modelValue"]);
+defineEmits(["update:modelValue", "update:select"]);
 </script>
-
 <template>
-  <label :for="forId">{{ label }}</label>
+  <label :for="forId" :class="labelColor({ l_color, l_size })">{{
+    label
+  }}</label>
   <input
-    :id="forId"
+    required
     v-bind="$attrs"
+    :id="forId"
     :type="type"
     @input="(e) => $emit('update:modelValue', e.target.value)"
     :value="modelValue"
-    class="border p-1 rounded-md outline-none"
-    required
+    :class="input({ size, radius, color })"
   />
 </template>
