20e9c9f39a1570e74f8962631465f720a357d045
EcemAydn <ecemaydn15@gmail.com>
2023-04-03 16:50:52 +0300

    create footer component

---
+++ src/components/TableFooterComp.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ChevronLeft } from "lucide-vue-next";
+import { ChevronRight } from "lucide-vue-next";
+</script>
+<template>
+  <div
+    class="px-2 flex items-center justify-between gap-6 bg-white mx-4 lg:mx-0 rounded-md"
+  >
+    <div class="flex items-center">
+      <ChevronLeft></ChevronLeft>
+      <ChevronRight></ChevronRight>
+      <p class="text-xs md:text-sm">Showing 1-20 of 2290</p>
+    </div>
+    <slot name="action"></slot>
+  </div>
+</template>
