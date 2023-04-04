c28f338a37a1877be1d65d0fbfe7ce945e9b31c0
EcemAydn <ecemaydn15@gmail.com>
2023-04-04 13:06:24 +0300

    add avatar component

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -5,6 +5,12 @@ import ButtonComp from "../components/ButtonComp.vue";
 import HeaderComp from "../components/HeaderComp.vue";
 import TableFooterComp from "../components/TableFooterComp.vue";
 import { useUserStore } from "../stores/user.js";
+import { Edit } from "lucide-vue-next";
+import { Trash2 } from "lucide-vue-next";
+import { Plus } from "lucide-vue-next";
+import { ChevronLeft } from "lucide-vue-next";
+import { ChevronRight } from "lucide-vue-next";
+import AvatarComp from "../components/AvatarComp.vue";
 
 const userStore = useUserStore();
 
@@ -21,7 +27,11 @@ const headers = ref([
     <HeaderComp title="All users">
       <template #actions>
         <div class="flex gap-2">
-          <ButtonComp button-name="+ Add User" color="secondary" />
+          <ButtonComp button-name="Add User" color="secondary">
+            <template #append>
+              <Plus class="w-4 h-4" />
+            </template>
+          </ButtonComp>
           <ButtonComp button-name="Export" color="warn" />
         </div>
       </template>
@@ -29,10 +39,7 @@ const headers = ref([
     <TableComp selectable :headers="headers" :items="userStore.users">
       <template #name="{ item }">
         <div class="flex items-center gap-2">
-          <img
-            :src="item.avatar"
-            class="w-12 h-12 lg:w-10 lg:h-10 rounded-lg"
-          />
+          <AvatarComp :image-link="item.avatar" :avatar-text="item.name" />
           <div>
             <p class="font-bold">{{ item.name }}</p>
             {{ item.email }}
@@ -41,16 +48,32 @@ const headers = ref([
       </template>
       <template #actions>
         <div class="flex justify-center gap-2">
-          <ButtonComp button-name="Edit User" />
-          <ButtonComp button-name="Delete User" color="warn" />
+          <ButtonComp button-name="Edit User">
+            <template #append>
+              <Edit class="w-4 h-4" />
+            </template>
+          </ButtonComp>
+          <ButtonComp button-name="Delete User" color="warn">
+            <template #append>
+              <Trash2 class="w-4 h-4" />
+            </template>
+          </ButtonComp>
         </div>
       </template>
     </TableComp>
     <TableFooterComp>
       <template #action>
         <div class="flex gap-2 py-2">
-          <ButtonComp button-name="Previous" color="secondary" size="xsmall" />
-          <ButtonComp button-name="Next" color="secondary" size="xsmall" />
+          <ButtonComp button-name="Previous" color="none" size="xsmall">
+            <template #append>
+              <ChevronLeft class="w-4 h-4" />
+            </template>
+          </ButtonComp>
+          <ButtonComp button-name="Next" color="none" size="xsmall">
+            <template #prepend>
+              <ChevronRight class="w-4 h-4" />
+            </template>
+          </ButtonComp>
         </div>
       </template>
     </TableFooterComp>
