85e8040360ddd6f03fbf1e66fde42898a182c25d
EcemAydn <ecemaydn15@gmail.com>
2023-04-05 18:06:42 +0300

    edit modal store

--- src/stores/modal.js
+++ src/stores/modal.js
@@ -1,17 +1,16 @@
-import { ref } from 'vue';
-import { defineStore } from 'pinia';
+import { ref } from "vue";
+import { defineStore } from "pinia";
 
+export const useModalStore = defineStore("modal", () => {
+  const modals = ref([]);
 
-export const useModalStore = defineStore('modal', () => {
-    const modals = ref([]);
-  
-    function addModal(modal){
-        modals.value.push(modal);
-    }
+  function addModal(modal) {
+    modals.value.push(modal);
+  }
 
-    function closeModal(){
-        modals.value.pop();
-    }
+  function closeModal() {
+    modals.value.pop();
+  }
 
-    return {modals, addModal, closeModal}
-})
+  return { modals, addModal, closeModal };
+});
