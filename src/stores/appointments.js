2958ce2d2fad3db116d37d2678bce94762456183
EcemAydn <ecemaydn15@gmail.com>
2023-04-05 18:03:41 +0300

    create appointment

--- src/stores/appointments.js
+++ src/stores/appointments.js
@@ -23,9 +23,36 @@ export const useAppointmentStore = defineStore("Appointment", () => {
         });
     });
   }
+  function createAppointment(item) {
+    return new Promise((resolve, reject) => {
+      api
+        .post("/appointments/create", {
+          customer_id: item.customer_id,
+          service_id: item.service_id,
+          staff_id: item.staff_id,
+          payment: item.payment,
+          duration: item.duration,
+          start_at: item.start_at,
+          status: item.status,
+        })
+        .then((response) => {
+          console.log(response.data);
+          appointments.value.push(response.data.appointments);
+          resolve("Successful");
+        })
+        .catch((error) => {
+          if (error.response) {
+            reject(error.response.data.message);
+          } else {
+            reject(error);
+          }
+        });
+    });
+  }
 
   return {
     getAppointments,
     appointments,
+    createAppointment,
   };
 });
